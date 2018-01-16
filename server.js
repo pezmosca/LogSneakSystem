230206a30fede7cc432c449320f14f31000ea776
Toni Ar <toniarellano96@gmail.com>
2018-01-16 17:03:26 +0100

    Creates a log file

--- server.js
+++ server.js
@@ -9,8 +9,20 @@ ws.on('connection', function(w){
   
   w.on('message', function(msg){
     d = new Date();
+    
+
+
     console.log(d + ': ' + msg);
+    var fs = require('fs');
+    var stream = fs.createWriteStream("server.log");
+      stream.once('open', function(fd) {
+      stream.write(d + ': ' + msg);
+    stream.end();
+    });
+
+    
     w.send('ACK: '+msg);
+
   });
   
   w.on('close', function() {

152d9f50b64605fe742ac5dc92393f95dc764e16
Toni Ar <toniarellano96@gmail.com>
2018-01-16 17:47:23 +0100

    Bugs corrected

--- server.js
+++ server.js
@@ -10,18 +10,19 @@ ws.on('connection', function(w){
   w.on('message', function(msg){
     const fs = require('fs');
 
+   
     //RECIVED LOG
     d = new Date();
     console.log(d + ': ' + msg);
-    fs.appendFile('recived.log', '\n' + d + ': ' + msg, (err) => {
-      if (err) throw err;
+    fs.appendFile('recived.log', '\n' + d + ': ' + msg, (err) => {  
+        if (err) throw err;
     });
 
     //SENT LOG
     d = new Date();
     w.send('ACK: ' + msg);
-    fs.appendFile('sent.log', '\n' + d + 'ACK: ' + msg, (err) => {
-       if (err) throw err;
+    fs.appendFile('sent.log', '\n' + d + 'ACK: ' + msg, (err2) => {
+       if (err2) throw err2;
      });
 
   });

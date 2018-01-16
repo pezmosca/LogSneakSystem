0a799ed6a4d1070facddf52b555200e9b24f5030
Toni Ar <toniarellano96@gmail.com>
2018-01-16 17:15:27 +0100

    creates a log file with append lines

--- server.js
+++ server.js
@@ -9,18 +9,15 @@ ws.on('connection', function(w){
   
   w.on('message', function(msg){
     d = new Date();
-    
-
-
     console.log(d + ': ' + msg);
-    var fs = require('fs');
-    var stream = fs.createWriteStream("server.log");
-      stream.once('open', function(fd) {
-      stream.write(d + ': ' + msg);
-    stream.end();
-    });
-
     
+    const fs = require('fs');
+
+   // add a line to a lyric file, using appendFile
+   fs.appendFile('server.log', '\n' + d + ': ' + msg, (err) => {  
+       if (err) throw err;
+   });
+
     w.send('ACK: '+msg);
 
   });
